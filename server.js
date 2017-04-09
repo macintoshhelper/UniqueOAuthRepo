0c2fc5fd2fd49640c24490a26a8c68e1354a1c8a
Mario <mario.saliba98@gmail.com>
2017-04-09 14:10:31 +0300

    fix https issue

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const hapi = require('hapi');
 const inert = require('inert');
+const fs = require('fs');
 
 const server = new hapi.Server();
 
+
+const options = {
+  key: fs.readFileSync('./keys/key.pem'),
+  cert: fs.readFileSync('./keys/cert.pem')
+}
 server.connection({
   port: process.env.PORT || 3000,
+  tls: options
 })
 
 server.register([inert], (err) => {
